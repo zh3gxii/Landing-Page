407bc3fd7bee8fd608acf465fb2191d5abfa715e
Peterson Olusegun <52452542+zh3gxii@users.noreply.github.com>
2022-07-26 13:30:28 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,54 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const navs = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+
+const navBuilder = () => {
+  let navUI = "";
+  
+  //section loops
+  sections.forEach((section) => {
+    const sectionID = section.id;
+    const sectionDataNav = section.dataset.nav;
+    navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+  });
+
+//Appends element to navigation
+  navs.innerHTML = navUI;
+}
+navBuilder();
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
